95ae7e52fa0b6615c6b832faa6754f894de6793e
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-27 23:46:13 +0530

    activity 10

---
+++ cspp2-practice/m1/a10/Helloworld.java
@@ -0,0 +1,16 @@
+/**
+    Java Hello World.
+*/
+public final class Helloworld {
+    /**
+        Hello World.
+    */
+    private Helloworld() {
+    }
+    /**
+    @param args to take args from command line
+    */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
