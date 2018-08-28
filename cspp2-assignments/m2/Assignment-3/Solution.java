791e16dd4d45141704101c86aa6fd7f87cb74e01
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-28 20:52:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * 
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner s = new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result=power(base,exponent);
+        System.out.println(result);
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      base      The base
+	 * @param      exponent  The exponent
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static long power(final int base, final int exponent) {
+		if (exponent == 0) {
+			return 1;
+		} else {
+			return base * power(base , exponent - 1);
+		}
+	}
+}
