26e1f9458f1f50f5a4818c2d3763ca123dc5d090
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-30 20:30:12 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -3,9 +3,33 @@ public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(a + "" + b);
-				
-	}
+		int r1 = sc.nextInt();
+		int c1 = sc.nextInt();
+		int[][] arr1 = new int[r1][c1];
+		for(int i =0 ; i<r1;i++) {
+			for(int j = 0; j<c1;j++) {
+				arr1[i][j] = sc.nextInt();
+			}
+		}
+
+		int r2 = sc.nextInt();
+		int c2 = sc.nextInt();
+		int[][] arr2 = new int[r2][c2];
+		for(int i =0 ; i<r2;i++) {
+			for(int j = 0; j<c2;j++) {
+				arr2[i][j] = sc.nextInt();
+			}
+		}
+
+		int[][] sum = new int[r1][c1];
+		for(int i =0 ; i<r2;i++) {
+			for(int j = 0; j<c2;j++) {
+				sum[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		for(int i =0 ; i<r2;i++) {
+			for(int j = 0; j<c2;j++) {
+				System.out.println(sum[i][j]);
+			}
+	}	}
 }
