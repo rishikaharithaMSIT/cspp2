30fe96a64edbf75c15483191a316a455d9d5019c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-30 20:09:08 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String one = sc.nextLine();
+		String[] size1 = one.split(" ", 1);
+		for (String a : size1)
+            System.out.println(a);
+				
+	}
+}
