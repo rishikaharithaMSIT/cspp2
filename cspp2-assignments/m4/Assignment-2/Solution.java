138d073148cac38eeabd9d0b01140dbc2b934939
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-30 20:36:53 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int r1 = sc.nextInt();
 		int c1 = sc.nextInt();
 		int[][] arr1 = new int[r1][c1];
-		for(int i =0 ; i<r1;i++) {
-			for(int j = 0; j<c1;j++) {
+		for (int i = 0 ; i < r1; i++) {
+			for (int j = 0; j < c1; j++) {
 				arr1[i][j] = sc.nextInt();
 			}
 		}
@@ -15,27 +15,28 @@ public class Solution {
 		int r2 = sc.nextInt();
 		int c2 = sc.nextInt();
 		int[][] arr2 = new int[r2][c2];
-		for(int i =0 ; i<r2;i++) {
-			for(int j = 0; j<c2;j++) {
+		for (int i = 0 ; i < r2; i++) {
+			for (int j = 0; j < c2; j++) {
 				arr2[i][j] = sc.nextInt();
 			}
 		}
-		if ((r1 != r2) || (c1 != c2)){
+		if ((r1 != r2) || (c1 != c2)) {
 			System.out.println("not possible");
 			return;
 		}
 		int[][] sum = new int[r1][c1];
-		for(int i =0 ; i<r2;i++) {
-			for(int j = 0; j<c2;j++) {
+		for (int i = 0 ; i < r2; i++) {
+			for (int j = 0; j < c2; j++) {
 				sum[i][j] = arr1[i][j] + arr2[i][j];
 			}
 		}
-		for(int i =0 ; i<r2;i++) {
-			for(int j = 0; j<c2-1;j++) {
-				System.out.print(sum[i][j]+ " ");
+		for (int i = 0 ; i < r2; i++) {
+			for (int j = 0; j < c2 - 1; j++) {
+				System.out.print(sum[i][j] + " ");
 			}
-			System.out.print(sum[i][c2-1]);
+			System.out.print(sum[i][c2 - 1]);
 			System.out.println();
-	}	}
+		}
+	}
 
 }
