ec792ac8aa18e2b51a52918d427aa64098324d56
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-30 20:33:31 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -20,7 +20,10 @@ public class Solution {
 				arr2[i][j] = sc.nextInt();
 			}
 		}
-
+		if ((r1 != r2) || (c1 != c2)){
+			System.out.println("not possible");
+			return;
+		}
 		int[][] sum = new int[r1][c1];
 		for(int i =0 ; i<r2;i++) {
 			for(int j = 0; j<c2;j++) {
@@ -29,7 +32,8 @@ public class Solution {
 		}
 		for(int i =0 ; i<r2;i++) {
 			for(int j = 0; j<c2;j++) {
-				System.out.println(sum[i][j]);
+				System.out.print(sum[i][j]);
 			}
 	}	}
+
 }
