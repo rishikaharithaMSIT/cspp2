2d9d83888d14fffaaf6a50c190677ead44939240
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-31 14:46:33 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);
-		System.out.println(reverse);
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static String reverseString(final String s) {
-		StringBuilder sb = new StringBuilder(s);
-		sb.reverse();
-		return sb.toString();
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(final String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        return sb.toString();
 
-	}
+    }
 
 }
