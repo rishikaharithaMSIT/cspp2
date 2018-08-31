b0c3f68d542ced1574f2460efebc1529115fa728
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-31 10:23:03 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -19,8 +19,9 @@ public class Solution
 		for (int i = 1; i <= s.length();i++){
 			//System.out.println(s.charAt(i));
 			decimal += (int) Math.pow(2,s.length()-i) * Character.getNumericValue(s.charAt(i-1));
-			decimalDigit += Integer.toString(decimal);
+			
 		}
+		decimalDigit = Integer.toString(decimal);
 		return decimalDigit;
 
 	}
