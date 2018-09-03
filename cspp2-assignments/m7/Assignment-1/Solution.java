7b5497e31b9f39d13d18d12e151c805ca40eb570
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-03 10:31:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -41,7 +41,7 @@ public class Solution {
      *
      * @param     args input from cmd.
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
         InputValidator i = new InputValidator(input);
