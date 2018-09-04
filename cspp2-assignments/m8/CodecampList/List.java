16f9f4f27f174874520005df1a56941e4cf95a12
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-04 16:51:00 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m8/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/List.java
@@ -343,7 +343,7 @@ public class List {
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
             default :
-                
+
             }
         }
     }
