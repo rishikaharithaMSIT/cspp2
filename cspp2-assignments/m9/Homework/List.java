15db89902e029f7b75209686c4b2314e9bee15c8
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-05 23:48:25 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m9/Homework/List.java
+++ cspp2-assignments/m9/Homework/List.java
@@ -304,8 +304,10 @@ public class List {
         for (int i = 0; i < size;) {
             if (i == index) {
                 list[ind] = item;
+                System.out.println(list[ind]+" in if");
             } else {
                 list[ind] = li[i];
+                System.out.println(list[ind]+" in else");
                 i++;
             }
             ind++;
