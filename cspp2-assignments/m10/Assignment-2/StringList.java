1f8dcd74bb5ea9f9c81f73261d519af03e038405
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-06 10:28:51 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/StringList.java
+++ cspp2-assignments/m10/Assignment-2/StringList.java
@@ -139,7 +139,9 @@ public class StringList implements StringListInterface{
         //System.out.println(Arrays.toString(items) + " list");
 		for (int i=0;i<items.length;i++) {            
             try {
+                if (items[i] != null){
                 list[size++] = items[i];
+            }
                 //System.out.println(Arrays.toString(list) + " list");
             }catch (Exception e){
                 resize();
