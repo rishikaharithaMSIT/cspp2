e2118dae92061fa38017539d834c02e1ff07f078
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-06 18:02:13 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -450,9 +450,10 @@ public class List {
                     if (t.length == 1) {
                         l.add(Integer.parseInt(tokens[1]));
                     } else {
-                        if (t.length > 1)
+                        if (t.length > 1) {
                             l.add(Integer.parseInt(t[0]),
                                   Integer.parseInt(t[1]));
+                        }
                     }
                 }
                 break;
@@ -462,9 +463,10 @@ public class List {
             case "addAll":
                 if (tokens.length == 2) {
                     String[] t1 = tokens[1].split(",");
-                    int temp[] = new int[t1.length];
-                    for (int i = 0; i < temp.length; i++)
+                    int[] temp = new int[t1.length];
+                    for (int i = 0; i < temp.length; i++) {
                         temp[i] = Integer.parseInt(t1[i]);
+                    }
                     l.addAll(temp);
                 }
                 break;
@@ -492,7 +494,7 @@ public class List {
             case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
-            default:    
+            default:
             }
         }
     }
