749092dc6032c18d08672cde65d21e05bb78a73f
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-07 14:48:01 +0530

    "check_style_score -> 1 of 8 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-2/List.java
+++ cspp2-assignments/m11/Assignment-2/List.java
@@ -246,7 +246,7 @@ public class List<E> {
     public boolean equals(List<E> li)
     {
         
-        for(int i =0;i<size;i++){
+        for(int i =0;i<li.size;i++){
             int cnt = 0;
             List lis = subList(i,i+li.size);
             System.out.println(Arrays.toString(lis.list) + " listt");
