5ee6f50b1e9a5152a2bd09b352f55acf9a015e81
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-08 10:21:11 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m13/Assignment-1/Solution.java
+++ cspp2-assignments/m13/Assignment-1/Solution.java
@@ -23,7 +23,7 @@ class Set {
         try{
         set[size] = item;
         size++;
-        //System.out.println(Arrays.toString(set));
+        System.out.println(Arrays.toString(set));
         }catch (Exception e){
             resize();
         }
