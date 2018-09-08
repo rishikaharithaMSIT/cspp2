3d2b2ee8bf9e8655b478ebc03dfcb3a0a9ba71aa
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-08 10:50:01 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m13/Assignment-1/Solution.java
+++ cspp2-assignments/m13/Assignment-1/Solution.java
@@ -61,12 +61,9 @@ class Set {
     }
     Set intersection(Set items) {
         Set intersectionSet = new Set();
-        //System.out.println(Arrays.toString(items.set) + " set");
         for (int i =0;i<size;i++) {
-            for(int j=0;j<items.size;j++) {
-                //System.out.println(set[i] +" - "+items.set[i]);
+            for(int j=0;j<items.size;j++) {                
                 if(set[i] == items.set[j]) {
-                    //System.out.println("in if" );
                     intersectionSet.add(items.set[j]);
             }
             }
