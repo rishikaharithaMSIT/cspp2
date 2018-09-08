8243bad2b1838e7dccca4ffd51bcceef6662e3d5
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-08 11:01:51 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m13/Assignment-1/Solution.java
+++ cspp2-assignments/m13/Assignment-1/Solution.java
@@ -85,12 +85,14 @@ class Set {
     }
     int[][] cartesianProduct(Set items) {
         int[][] product = new int[items.size*size][2];
+        for(int k =0;k< items.size*size;k++) {
         for (int i =0;i<size;i++) {
             for(int j=0;j<items.size;j++) {                
                 product[i][0] = set[i];
                 product[i][1] = set[j];
             }
             }
+        }
         return product;
     }
 
