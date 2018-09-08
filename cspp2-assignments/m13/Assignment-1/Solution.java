3e77a6d7c57e7b39b4d124e69e237748aeb14636
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-08 11:03:54 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m13/Assignment-1/Solution.java
+++ cspp2-assignments/m13/Assignment-1/Solution.java
@@ -88,8 +88,8 @@ class Set {
         for(int k =0;k< items.size*size;k++) {
         for (int i =0;i<size;i++) {
             for(int j=0;j<items.size;j++) {                
-                product[i][0] = set[i];
-                product[i][1] = set[j];
+                product[k][0] = set[i];
+                product[k][1] = set[j];
             }
             }
         }
