8fdef80923a58fb3bdb345c6be3841e3090fc812
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-12 10:43:10 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m16/bookyourshow/Solution.java
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -20,15 +20,16 @@ class Solution {
             String line = stdin.nextLine();
             String[] keys = line.split(" ",2);
             System.out.println(keys[0] + "keys 0 " );
-            System.out.println(keys[1] + "keys 1" );
+            
+            // System.out.println(keys[1] + "keys 1" );
             // String[] tokens = keys[1].split(",", 3);
-            // switch(keys[0]) {
-            // 	case "add":
-            	
-            // 	System.out.println(tokens[0] + "0");
-            // 	System.out.println(tokens[1] + "1");
-            // 	System.out.println(tokens[2] + "2");
-            // }
+            switch(keys[0]) {
+            	case "add":
+            	System.out.println(keys[1] + "keys 1" );
+            	// System.out.println(tokens[0] + "0");
+            	// System.out.println(tokens[1] + "1");
+            	// System.out.println(tokens[2] + "2");
+            }
         }
 
  }
