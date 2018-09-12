919773be77690e0ca220b246ce7af300e32e0e4c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-12 11:01:00 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m16/bookyourshow/Solution.java
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -2,6 +2,16 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 class Show {
+	String movieName;
+	String dateTime;
+	String seats;
+	Show(String movieName, String dateTime, String seats){
+		this.movieName = movieName;
+		this.dateTime = dateTime;
+		this.seats = seats;
+		System.out.println(seats.replace("[",""));
+		System.out.println(seats.replace("]",""));
+	}
 
 }
 
@@ -10,7 +20,11 @@ class Patron {
 }
 
 class BookYourShow {
+void addAShow(String inp) {
+	String[] tokens = inp.split(",", 3);
+	Show show = new Show(tokens[0],tokens[1],tokens[2]);
 
+}
 }
 
 class Solution {
@@ -20,15 +34,17 @@ class Solution {
             String line = stdin.nextLine();
             String[] keys = line.split(" ",2);
             System.out.println(keys[0] + " keys 0 " );
-
+            BookYourShow bys = new BookYourShow();
             // System.out.println(keys[1] + "keys 1" );
             // String[] tokens = keys[1].split(",", 3);
             switch(keys[0]) {
             	case "add":
-            	System.out.println(keys[1] + " keys 1" );
-            	// System.out.println(tokens[0] + "0");
-            	// System.out.println(tokens[1] + "1");
-            	// System.out.println(tokens[2] + "2");
+            	bys.addAShow(keys[1]);
+            	// String[] tokens = keys[1].split(",", 3);
+            	// System.out.println(tokens[0] + " t 0" );
+            	// System.out.println(tokens[1] + " t 1" );
+            	// System.out.println(tokens[2] + " t 2" );
+            	
             }
         }
 
