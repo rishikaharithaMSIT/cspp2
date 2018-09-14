eff2ea750e1749976804f88b81c4e3c0f7753c39
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-14 14:40:32 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.ArrayList;
-class Item{
+class Item {
 	String productName;
 	String quantity;
 	float unitPrice;
@@ -18,80 +18,80 @@ class ShoppingCart {
 	static int apply = 0;
 	void addToCatalog(Item item) {
 		//System.out.println("hi "+ item.productName);
-		catalog.add(item);	
+		catalog.add(item);
 	}
 	void showCatalog() {
 		//System.out.println(catalog.size());
-		for(int i=0; i < catalog.size();i++) {
-			System.out.println(catalog.get(i).productName + " "+
-				catalog.get(i).quantity + " " + catalog.get(i).unitPrice);
+		for (int i = 0; i < catalog.size(); i++) {
+			System.out.println(catalog.get(i).productName + " " +
+			                   catalog.get(i).quantity + " " + catalog.get(i).unitPrice);
 		}
 	}
 	void showCart() {
 		//System.out.println(catalog.size());
-		for(int i=0; i < cart.size();i++) {
-			System.out.println(cart.get(i).productName + " "+
-				cart.get(i).inCartQuantity);
+		for (int i = 0; i < cart.size(); i++) {
+			System.out.println(cart.get(i).productName + " " +
+			                   cart.get(i).inCartQuantity);
 		}
 	}
 	void addToCart(Item item) {
-		cart.add(item); 
+		cart.add(item);
 
 	}
 	void removeFromCart(Item item, int quan) {
-			int cartQuan = Integer.parseInt(item.inCartQuantity);
-			if(quan < cartQuan){
-				//System.out.println(item.productName + " in remove");
-				item.inCartQuantity = (cartQuan - quan) + "";
-			}else {
-				cart.remove(item);
-			}
-		
+		int cartQuan = Integer.parseInt(item.inCartQuantity);
+		if (quan < cartQuan) {
+			//System.out.println(item.productName + " in remove");
+			item.inCartQuantity = (cartQuan - quan) + "";
+		} else {
+			cart.remove(item);
+		}
+
 	}
 	float getTotalAmount() {
 		float totalAmount = 0.0f;
-		for(int i=0;i<cart.size();i++) {
+		for (int i = 0; i < cart.size(); i++) {
 			int quantity = Integer.parseInt(cart.get(i).inCartQuantity);
 			float unitPrice = cart.get(i).unitPrice;
-			totalAmount += (quantity*unitPrice);
+			totalAmount += (quantity * unitPrice);
 		}
 		return totalAmount;
-		
+
 	}
 	float getPayableAmount() {
 		float total = 0.0f;
 		float tax = 0.0f;
-		float discount = (apply/100.0f)*total;
-		for(int i=0;i<cart.size();i++) {
+		float discount = (apply / 100.0f) * total;
+		for (int i = 0; i < cart.size(); i++) {
 			int quantity = Integer.parseInt(cart.get(i).inCartQuantity);
 			float unitPrice = cart.get(i).unitPrice;
-			total += (quantity*unitPrice);
+			total += (quantity * unitPrice);
 		}
 		//System.out.println("Total"+totalAmount);
-		tax = 0.15f*(total-discount);
-		float totalAmount = total+tax-discount;
+		tax = 0.15f * (total - discount);
+		float totalAmount = total + tax - discount;
 		return totalAmount;
 	}
 	void printInvoice() {
-		
+
 		float total = getTotalAmount();
-		float discount = (apply/100.0f)*total;
-		float tax = 0.15f*(total-discount);
+		float discount = (apply / 100.0f) * total;
+		float tax = 0.15f * (total - discount);
 		System.out.println("Name   quantity   Price");
-		for(int i=0;i<cart.size();i++) {
-			System.out.println(cart.get(i).productName + " "+cart.get(i).inCartQuantity+" "+cart.get(i).unitPrice);
+		for (int i = 0; i < cart.size(); i++) {
+			System.out.println(cart.get(i).productName + " " + cart.get(i).inCartQuantity + " " + cart.get(i).unitPrice);
 		}
 		//System.out.println("totalAmount: "+total);
-		System.out.println("Total:"+total);
-		System.out.println("Disc%:"+discount);
-		System.out.println("Tax:"+tax);
-		System.out.println("Payable amount: "+getPayableAmount());
+		System.out.println("Total:" + total);
+		System.out.println("Disc%:" + discount);
+		System.out.println("Tax:" + tax);
+		System.out.println("Payable amount: " + getPayableAmount());
 	}
 	boolean applyCoupon(String coupon) {
 		String[] coupons = coupon.split("D");
 		//System.out.println("couponnnnnnnnnnnnnnnnnnnnnnnn" + coupons[1]);
 		int cou =  Integer.parseInt(coupons[1]);
-		if(cou == 10|| cou == 20 || cou == 30 || cou == 50) {
+		if (cou == 10 || cou == 20 || cou == 30 || cou == 50) {
 			apply = cou;
 			return true;
 		}
@@ -103,72 +103,72 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
 		boolean couponApplied = false;
-		while(stdin.hasNext()) {
+		while (stdin.hasNext()) {
 			ShoppingCart sc = new ShoppingCart();
 			String line = stdin.nextLine();
 			String[] keys = line.split(" ");
-			
-			switch(keys[0]) {
-				case "Item":
-					String[] tokens = keys[1].split(",");
-					Item item = new Item(tokens[0], tokens[1], tokens[2]);
-					sc.addToCatalog(item);
-					items.add(item);
-					break;
-				case "add":
-					tokens = keys[1].split(",");
-					//System.out.println(items.size());
-					for(int i=0;i<items.size();i++) {
-						//System.out.println(items.get(i).productName +" "+ tokens[0]);
-						//System.out.println(items.get(i).quantity +" "+ tokens[1]);
-						//System.out.println(items.get(i).unitPrice +" "+ tokens[2]);
 
-						if(items.get(i).productName.equals(tokens[0]) &&
-							Integer.parseInt(items.get(i).quantity) >= Integer.parseInt(tokens[1])){
-							items.get(i).inCartQuantity = tokens[1];
-							sc.addToCart(items.get(i));
-							break;
-						}
-					}
-					//addToCart(item);
-					break;
-				case "show":
-					sc.showCart();
-					break;
-				case "catalog":
-					sc.showCatalog();
-					break;
-				case "remove":
-					tokens = keys[1].split(",");
-					for(int i=0;i<items.size();i++) {
-						if(items.get(i).productName.equals(tokens[0])){
-							//System.out.println(items.get(i).productName + " " +tokens[0]);
-							sc.removeFromCart(items.get(i), 
-								Integer.parseInt(tokens[1]));
-							break;
-						}
-					}
-					break;
-				case "totalAmount":
-					System.out.println("totalAmount: "+sc.getTotalAmount());
-					break;
-				case "payableAmount":
-					System.out.println("Payable amount: "+sc.getPayableAmount());
-					break;
-				case "coupon":
+			switch (keys[0]) {
+			case "Item":
+				String[] tokens = keys[1].split(",");
+				Item item = new Item(tokens[0], tokens[1], tokens[2]);
+				sc.addToCatalog(item);
+				items.add(item);
+				break;
+			case "add":
+				tokens = keys[1].split(",");
+				//System.out.println(items.size());
+				for (int i = 0; i < items.size(); i++) {
+					//System.out.println(items.get(i).productName +" "+ tokens[0]);
+					//System.out.println(items.get(i).quantity +" "+ tokens[1]);
+					//System.out.println(items.get(i).unitPrice +" "+ tokens[2]);
 
-					if(couponApplied == false) {
-						if(sc.applyCoupon(keys[1])) {
-							couponApplied = true;
-						}else {
-							System.out.println("Invalid coupon");
-						}
+					if (items.get(i).productName.equals(tokens[0]) &&
+					        Integer.parseInt(items.get(i).quantity) >= Integer.parseInt(tokens[1])) {
+						items.get(i).inCartQuantity = tokens[1];
+						sc.addToCart(items.get(i));
+						break;
 					}
-					break;
-				case "print":
-					sc.printInvoice();
-					break;
-					
+				}
+				//addToCart(item);
+				break;
+			case "show":
+				sc.showCart();
+				break;
+			case "catalog":
+				sc.showCatalog();
+				break;
+			case "remove":
+				tokens = keys[1].split(",");
+				for (int i = 0; i < items.size(); i++) {
+					if (items.get(i).productName.equals(tokens[0])) {
+						//System.out.println(items.get(i).productName + " " +tokens[0]);
+						sc.removeFromCart(items.get(i),
+						                  Integer.parseInt(tokens[1]));
+						break;
+					}
+				}
+				break;
+			case "totalAmount":
+				System.out.println("totalAmount: " + sc.getTotalAmount());
+				break;
+			case "payableAmount":
+				System.out.println("Payable amount: " + sc.getPayableAmount());
+				break;
+			case "coupon":
+
+				if (couponApplied == false) {
+					if (sc.applyCoupon(keys[1])) {
+						couponApplied = true;
+					} else {
+						System.out.println("Invalid coupon");
+					}
+				}
+				break;
+			case "print":
+				sc.printInvoice();
+				break;
+
 			}
 		}
 	}
