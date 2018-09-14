ce798de43534d3710d0f8809bae382aaa1ea5da0
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-14 11:56:15 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -40,10 +40,8 @@ class ShoppingCart {
 	void removeFromCart(Item item, int quan) {
 		for(int i =0;i<cart.size();i++) {
 			int cartQuan = Integer.parseInt(cart.get(i).inCartQuantity);
-			int totalQuan = Integer.parseInt(cart.get(i).quantity);
 			if(quan < cartQuan){
 				cart.get(i).inCartQuantity = (cartQuan - quan) + "";
-				cart.get(i).quantity = (totalQuan - quan) +"";
 				break;
 			}else {
 				cart.remove(cart.get(i));
