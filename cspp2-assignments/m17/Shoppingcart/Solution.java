8bbc5780cfce2e1af54464f573bdcd515b961f30
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-14 09:52:12 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -1,18 +1,37 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.ArrayList;
-
+class Item{
+	String productName;
+	String quantity;
+	String unitPrice;
+	Item(String productName, String quantity, String unitPrice) {
+		this.productName = productName;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+}
 class ShoppingCart {
-
+	void addToCatalog(Item item) {
+		System.out.println("hi "+ item.productName);
+	}
 }
 class Solution {
 	public static void main(String[] args) {
 		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
 		while(stdin.hasNext()) {
+			ShoppingCart sc = new ShoppingCart();
 			String line = stdin.nextLine();
 			String[] keys = line.split(" ");
 			System.out.println(keys[0]);
-			//System.out.println(keys[1]);
+			switch(keys[0]) {
+				case "Item":
+					String[] tokens = keys[1].split(",");
+					Item item = new Item(tokens[0], tokens[1], tokens[2]);
+					sc.addToCatalog(item);
+
+
+			}
 		}
 	}
 }
