56b3543829842b6429efddc9cd42a0afd5377195
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-14 13:53:35 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -91,7 +91,7 @@ class ShoppingCart {
 		System.out.println("Total:"+total);
 		System.out.println("Disc%:"+discount);
 		System.out.println("Tax:"+tax);
-		System.out.println("Payable amount: "+getPayableAmount());
+		System.out.println("Payable amount: "+(getPayableAmount()-discount));
 	}
 	void applyCoupon(String coupon) {
 		String[] coupons = coupon.split("D");
