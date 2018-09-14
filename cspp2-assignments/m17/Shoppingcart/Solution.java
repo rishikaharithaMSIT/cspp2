c45b271ff8d4f7f905ccd2027f6d0b538e7d089d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-14 14:15:35 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -75,7 +75,7 @@ class ShoppingCart {
 	void printInvoice() {
 		
 		float total = getTotalAmount();
-		float discount = (apply/100.0)*total;
+		float discount = (apply/100.0f)*total;
 		float tax = 0.15f*total;
 		System.out.println("Name   quantity   Price");
 		for(int i=0;i<catalog.size();i++) {
