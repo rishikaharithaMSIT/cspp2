1969198900dbed6be4742ccf8726f642a9d4297d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-15 11:54:24 +0530

    "check_style_score -> 4 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -167,9 +167,9 @@ public final class Solution {
         int finalScore = 0;
         for(int i=0;i<quizes.size();i++) {
             System.out.println(quizes.get(i).question);
-            System.out.println(answers.get(i) + " - "+ quizes.get(i).correct);
-                String[] an = quizes.get(i).choices[i].split(" ");
-                if(answers.get(i).equals(quizes.get(i).correct) || answers.get(i).equals(an[1]) ){
+            //System.out.println(answers.get(i) + " - "+ quizes.get(i).correct);
+                //String[] an = quizes.get(i).choices[i].split(" ");
+                if(answers.get(i).equals(quizes.get(i).correct)){
                     
                     System.out.println(" Correct Answer! - Marks Awarded: "+quizes.get(i).maxMarks);
                     finalScore += Integer.parseInt(quizes.get(i).maxMarks);
