bf8a784adef78941ccfb9ed39fecf989ecb2a348
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-17 11:38:23 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -4,54 +4,53 @@ import java.util.Scanner;
  * Solution class for code-eval.
  */
 public final class Solution {
-	/**
-	* Constructs the object.
-	*/
-	private Solution() {
-		// leave this blank
-	}
-	/**
-	 * main function to execute test cases.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		// instantiate this Quiz
-		Quiz q = new Quiz();
-		// code to read the test cases input file
-		ProcessQuiz pq = new ProcessQuiz();
-		Scanner s = new Scanner(System.in);
-		// check if there is one more line to process
-		while (s.hasNext()) {
-			// read the line
-			String line = s.nextLine();
-			// split the line using space
-			String[] tokens = line.split(" ");
-			// based on the list operation invoke the corresponding method
-			switch (tokens[0]) {
-			case "LOAD_QUESTIONS":
-				System.out.println("|----------------|");
-				System.out.println("| Load Questions |");
-				System.out.println("|----------------|");
-				pq.loadQuestions(s, q, Integer.parseInt(tokens[1]));
-				break;
-			case "START_QUIZ":
-				System.out.println("|------------|");
-				System.out.println("| Start Quiz |");
-				System.out.println("|------------|");
-				pq.startQuiz(s, q, Integer.parseInt(tokens[1]));
-				break;
-			case "SCORE_REPORT":
-				System.out.println("|--------------|");
-				System.out.println("| Score Report |");
-				System.out.println("|--------------|");
-				pq.displayScore(q);
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    /**
+    * Constructs the object.
+    */
+    private Solution() {
+        // leave this blank
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // instantiate this Quiz
+        Quiz q = new Quiz();
+        // code to read the test cases input file
+        ProcessQuiz pq = new ProcessQuiz();
+        Scanner s = new Scanner(System.in);
+        // check if there is one more line to process
+        while (s.hasNext()) {
+            // read the line
+            String line = s.nextLine();
+            // split the line using space
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "LOAD_QUESTIONS":
+                System.out.println("|----------------|");
+                System.out.println("| Load Questions |");
+                System.out.println("|----------------|");
+                pq.loadQuestions(s, q, Integer.parseInt(tokens[1]));
+                break;
+            case "START_QUIZ":
+                System.out.println("|------------|");
+                System.out.println("| Start Quiz |");
+                System.out.println("|------------|");
+                pq.startQuiz(s, q, Integer.parseInt(tokens[1]));
+                break;
+            case "SCORE_REPORT":
+                System.out.println("|--------------|");
+                System.out.println("| Score Report |");
+                System.out.println("|--------------|");
+                pq.displayScore(q);
+                break;
+            default:
+                break;
+            }
+        }
+    }
 
 
 }
