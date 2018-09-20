d6e21271b7d188211ea0f3667a4a5e2c7815be56
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 17:10:47 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -9,6 +9,9 @@ public class Solution {
 			String line = scan.nextLine();
 			File dir = new File(line);
 			File[] files = dir.listFiles();
+			int maxVal = 0;
+			String file1 = null;
+			String file2 = null; 
 			System.out.print("		");
 			for(int i=0;i<files.length-1;i++) {
 				System.out.print(files[i].getName()+"\t");
@@ -20,13 +23,19 @@ public class Solution {
 				for(int j =0;j < files.length;j++) {
 					Distance map1 = new Distance(files[i]);
 					Distance map2 = new Distance(files[j]);
-					System.out.print(Math.round(map1.angle(map2))+ "\t\t");
+					int similar = (int)Math.round(map1.angle(map2));
+					if(similar > maxVal && !files[i].getName().equals(files[j].getName())) {
+						maxVal = similar;
+						file1 = files[i].getName();
+						file2 = files[j].getName();
+					}
+					System.out.print(similar+ "\t\t");
 				}
-				System.out.println();
+				System.out.println();				
 				
 			}
+			System.out.println("Maximum similarity is between "+file1+" and "+file2);
 			
-
 			
 		} catch(Exception e) {
 			System.out.println("empty directory");
