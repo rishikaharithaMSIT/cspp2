2a3631e992a84be55987e2eccfdcce9179da141d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 16:21:22 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -7,14 +7,13 @@ public class Solution {
 		try {
 			Scanner scan = new Scanner(System.in);
 	
-			while(scan.hasNext()) {
 			String line = scan.nextLine();
 			File dir = new File(line);
 			File[] files = dir.listFiles();
 			//System.out.println(Arrays.toString(files));
 			//if(files.length == 0) throw new Exception();
 
-			}
+			
 		} catch(Exception e) {
 			System.out.println("empty directory");
 		}
