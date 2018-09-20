49206b3dee097895c932117bec9d1a65b192cb20
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 20:24:39 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 import java.io.File;
-public class Solution {
+/**
+ * Solution Class.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         try {
             Scanner scan = new Scanner(System.in);
@@ -10,7 +24,7 @@ public class Solution {
             int maxVal = 0;
             String file1 = null;
             String file2 = null;
-            System.out.print("      ");
+            System.out.print("\t\t");
             for (int i = 0; i < files.length - 1; i++) {
                 System.out.print(files[i].getName() + "\t");
             }
