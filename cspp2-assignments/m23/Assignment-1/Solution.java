86f70cea512ae8f07e89693b6f68b5547b3c7582
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 16:42:35 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -5,13 +5,19 @@ import java.io.*;
 public class Solution {
 	public static void main(String[] args) {
 		try {
-			Scanner scan = new Scanner(System.in);
-	
+			Scanner scan = new Scanner(System.in);	
 			String line = scan.nextLine();
 			File dir = new File(line);
 			File[] files = dir.listFiles();
-			//System.out.println(Arrays.toString(files));
-			//if(files.length == 0) throw new Exception();
+			for(int i =0;i < files.length-1;i++) {
+				for(int j =0;j < files.length-1;j++) {
+					Distance map1 = new Distance(files[0]);
+					Distance map2 = new Distance(files[1]);
+					System.out.println(map1.angle(map2) + " : " + files[0] + " , "+ files[1]);
+				}
+				
+			}
+			
 
 			
 		} catch(Exception e) {
