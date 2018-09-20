2927428d1c8a26032362355c84db5fb8e573800b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 16:18:30 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -5,11 +5,17 @@ import java.io.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()) {
-			String line = scan.nextLine();
-			File dir = new File(line);
-			File[] files = dir.listFiles();
-			System.out.println(Arrays.toString(files));
+		try {
+				while(scan.hasNext()) {
+				String line = scan.nextLine();
+				File dir = new File(line);
+				File[] files = dir.listFiles();
+				//System.out.println(Arrays.toString(files));
+
+			}
+		} catch(Exception e) {
+			System.out.println("empty directory");
 		}
+		
 	}
 }
