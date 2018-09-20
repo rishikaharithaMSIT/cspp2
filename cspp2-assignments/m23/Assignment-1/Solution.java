14fde48dce9eabf4a20866d5e28450e725530a27
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-20 15:47:25 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext()) {
+			String line = scan.nextLine();
+			System.out.println(line);
+		}
+	}
+}
