3c9da692c6ab5fd3b745d68cdd656d2bcb95165d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-22 11:28:26 +0530

    "check_style_score -> 14 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -72,35 +72,7 @@ class Todoist {
     }
 
 }
-class Task {
-	String title;
-	String assignedTo;
-	int timeToComplete;
-	boolean important;
-	boolean urgent;
-	String status;
 
-	Task(String title, String assignedTo, int timeToComplete, boolean important,
-	     boolean urgent, String status) throws Exception {
-		if (title.equals("") || title == null) throw new Exception("Title not provided");
-		if (timeToComplete < 0) throw new Exception("Invalid timeToComplete " + timeToComplete);
-		if (!status.equals("todo") && !status.equals("done")) throw new Exception("Invalid status " + status);
-		this.title = title;
-		this.assignedTo = assignedTo;
-		this.timeToComplete = timeToComplete;
-		this.important = important;
-		this.urgent = urgent;
-		this.status = status;
-	}
-	public String toString() {
-		String imp = "Not Important";
-		String urg = "Not Urgent";
-		if(important) imp = "Important";
-		if(urgent) urg = "Urgent";
-		String display = title + ", " + assignedTo+", "+ timeToComplete+", "+ imp+", "+ urg+", "+ status;
-		return display;
-	}
-}
 /**
  * Class for todoist main.
  */
