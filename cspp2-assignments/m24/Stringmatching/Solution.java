fad07bf5c5576a2372be0371b2c72faf4ea2d8d4
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-23 16:06:02 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Stringmatching/Solution.java
+++ cspp2-assignments/m24/Stringmatching/Solution.java
@@ -46,6 +46,7 @@ public final class Solution {
                     // System.out.print(" ");
                     //System.out.print(similar + "\t\t");
                 }
+                System.out.print(" ");
                 System.out.println();
             }
             System.out.println("Maximum similarity is between " + file1
