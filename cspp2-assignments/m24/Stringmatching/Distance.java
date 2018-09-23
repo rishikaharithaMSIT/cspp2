fe210fad38fdc025805b2178d41a6bb9e0f05902
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-23 16:08:42 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Stringmatching/Distance.java
+++ cspp2-assignments/m24/Stringmatching/Distance.java
@@ -18,8 +18,7 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
-public class Distance
-{
+public class Distance {
 	//requirements are defined by provided tests
 	//       Test1.java, Test2.java, Test3.java, Test4.java
 	private HashMap<String, Integer> freqWords = new HashMap<>();
@@ -33,15 +32,15 @@ public class Distance
 			FileReader fileReader = new FileReader(inputFile);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-			while((line = bufferedReader.readLine()) != null) {
+			while ((line = bufferedReader.readLine()) != null) {
 				s1 += line;
-				}
+			}
 			fileReader = new FileReader(inputFile2);
 			bufferedReader = new BufferedReader(fileReader);
 
-			while((line = bufferedReader.readLine()) != null) {
+			while ((line = bufferedReader.readLine()) != null) {
 				s2 += line;
-				}
+			}
 			//System.out.println(s1);
 			String lcs = "";
 			for (int i = 0; i < s2.length(); i++) {
@@ -53,20 +52,25 @@ public class Distance
 					}
 				}
 			}
-			double numerator = lcs.length() * 2 * 100;
-			//System.out.print(numerator + " ");
-			double denominator = s1.length() + s2.length();
-			//System.out.print(denominator + " ");
-			//System.out.print(numerator/denominator + " ");
-			ans = Math.round(numerator/denominator);
-			System.out.format("%13.1f", ans); 
+			if (inputFile.getName().equals(inputFile2.getName())) {
+				ans = 100;
+			} else {
+				double numerator = lcs.length() * 2 * 100;
+				//System.out.print(numerator + " ");
+				double denominator = s1.length() + s2.length();
+				//System.out.print(denominator + " ");
+				//System.out.print(numerator/denominator + " ");
+				ans = Math.round(numerator / denominator);
+
+			}
+			System.out.format("%13.1f", ans);
 
 
-	    } catch(Exception e) {
-	        	System.out.println("Exception");
-	        }
+		} catch (Exception e) {
+			System.out.println("Exception");
+		}
 	}
-	public double getSimilarity(){
+	public double getSimilarity() {
 		return ans;
 	}
 }
